a9cf20401aa3d62f57d35eab133e1909f8d8c7de
Crowdbotics <team@crowdbotics.com>
2022-07-06 17:16:16 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "air_force_1_36064",
+  url: "https://air_force_1_36064.botics.co",
+  api: "https://air_force_1_36064.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
